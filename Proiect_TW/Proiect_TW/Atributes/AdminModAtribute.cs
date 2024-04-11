c397a4fd8c7e6a5aaffbf65e407c55376851bb4f
Gheorghe Cristian <gheorghecristian612@gmail.com>
2024-04-11 12:59:43 +0300

    Am adaugat nivelele de acces pentru utilizator

---
+++ Proiect_TW/Proiect_TW/Atributes/AdminModAtribute.cs
@@ -0,0 +1,44 @@
+﻿using Proiect_TW.BusinessLogic;
+using Proiect_TW.Domain.Enums;
+using Proiect_TW.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proiect_TW.BusinessLogic.Interfaces;
+using System.Web.Routing;
+
+
+
+namespace Proiect_TW.Atributes
+{
+    public class AdminModAttribute: ActionFilterAttribute
+    {
+        private readonly ISession _sessionBusinessLogic;
+
+        public AdminModAttribute()
+        {
+            var bl = new BussinessLogic();
+            _sessionBusinessLogic = bl.GetSessionBL();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var apiCookie = HttpContext.Current.Request.Cookies["X-KEY"];
+            if (apiCookie != null)
+            {
+                var profile = _sessionBusinessLogic.GetUserByCookie(apiCookie.Value);
+                if (profile != null && profile.Level == URole.Admin) 
+                {
+                    HttpContext.Current.SetMySessionObject(profile);
+                }
+                else 
+                {
+                    filterContext.Result = new RedirectToRouteResult(new
+                        RouteValueDictionary(new { controller = "Error", action = "Error404" }));
+                }
+            }
+        }
+    }
+}
---
+++ Proiect_TW/Proiect_TW/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Proiect_TW.Atributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proiect_TW.Controllers
+{
+    public class AdminController : Controller
+    {
+        [AdminMod]
+        // GET: Admin
+        public ActionResult AddProduct()
+        {
+            if ((string)System.Web.HttpContext.Current.Session["LoginStatus"] != "login")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
