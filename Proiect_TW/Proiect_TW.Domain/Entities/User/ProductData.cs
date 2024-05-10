aa92f935dbb7b9dc07c525db04fee3384a78f494
Nichita Porcesco <nichita.03@mail.ru>
2024-05-10 09:31:38 -0700

    am adaugat productdata si productresp

---
+++ Proiect_TW/Proiect_TW.Domain/Entities/User/ProductData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_TW.Domain.Entities.User
+{
+    public class ProductData
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Style { get; set; }
+        public string Type { get; set; }
+        public string Ip { get; set; }
+        public DateTime PublishTime { get; set; }
+    }
+}
---
+++ Proiect_TW/Proiect_TW.Domain/Entities/User/ProductResp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_TW.Domain.Entities.User
+{
+    public class ProductResp
+    {
+        public bool Status { get; set; }
+        public string StatusMsg { get; set; }
+    }
+}
